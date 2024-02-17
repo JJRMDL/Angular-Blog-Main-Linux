3ae672cb70884f6e7dc0629b3e794e2fcaae5b06
root <root@Aspire-E1-572-ca95cd34.localdomain>
2024-02-17 16:38:48 -0300

    components createds

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Sistema Linux Elementary OS 7.1",
+    "description":"O que há de novo no elementary OS 7.1 Feito com cuidado e pensando em você. O OS 7.1 oferece novas opções de personalização que o tornam mais inclusivo e acessível, protege sua privacidade e garante que os aplicativos sempre funcionem com seu consentimento explícito, além de responder aos seus comentários com mais de 200 correções de bugs, alterações de design e novos recursos.",
+    "photoCover":"https://upload.wikimedia.org/wikipedia/commons/c/c7/Elementary_OS_5.1_Hera.png"
+  },
+  {
+    "id":"2",
+    "title": "Novo Ubuntu 23.10",
+    "description": "Ubuntu Desktop Rápido, gratuito e cheio de novidades. A versão mais recente do Ubuntu Desktop oferece novas ferramentas e melhorias para desenvolvedores, criadores, jogadores e administradores. Baixe o Ubuntu Desktop e substitua seu sistema operacional atual ou execute o Ubuntu junto com ele.",
+    "photoCover":"https://upload.wikimedia.org/wikipedia/commons/b/bd/Ubuntu_23.10_Mantic_Minotaur_Desktop_English.png"
+  },
+  {
+    "id":"3",
+    "title": "Fedora 39",
+    "description": "Uma área de trabalho linda, de alta qualidade, construída com as últimas tecnologias open source. Confiável, poderoso e fácil.",
+    "photoCover":"https://www.omgubuntu.co.uk/wp-content/uploads/2023/11/fedora-39.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Novo Zorin OS 17",
+    "description": "Zorin OS is the alternative to Windows and macOS designed to make your computer faster, more powerful, secure, and privacy-respecting.Zorin OS is designed to be easy, so you don't need to learn anything to get started. The Zorin Appearance app lets you change the desktop layout to feel like the environment you're familiar with, whether it's Windows, macOS, or Linux.",
+    "photoCover":"https://www.edivaldobrito.com.br/wp-content/uploads/2023/12/zorin-os-17-beta-lancado-com-o-gnome-43-kernel-6-2-e-mais-1.webp"
+  }
+]
